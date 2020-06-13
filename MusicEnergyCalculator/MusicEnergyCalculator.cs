31bbfab5561993b36a671496d39e22ca372c6990
Julian Lawrance <julian.lawrance@gmail.com>
2020-06-13 15:04:59 +0100

    Return summary of audio features of the track with the given ID

--- MusicEnergyCalculator/MusicEnergyCalculator.cs
+++ MusicEnergyCalculator/MusicEnergyCalculator.cs
@@ -1,35 +1,72 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Models;
 
 namespace MusicEnergyCalculator
 {
     public static class MusicEnergyCalculator
     {
+        private const string ClientId = "de8e2df8cbb84989bb42d13700e83168";
+        private const string ClientSecret = "b6ff30e7e1744da9ac435eda959fbbf4";
+
         [FunctionName("MusicEnergyCalculator")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            string trackParam = req.Query["track"];
+            var trackId = string.IsNullOrEmpty(trackParam) ? "31zBvESt331Jm2pU1R9tXt" : trackParam;
+            log.LogInformation($"Getting audio features for track: {trackId}");
 
-            string name = req.Query["name"];
+            var features = await GetAudioFeatures(trackId);
+            var summary = GetSummary(features);
+            return new OkObjectResult(summary);
+        }
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        /// <summary>
+        /// Returns audio feature data for the track with the given ID.
+        /// </summary>
+        private static string GetSummary(AudioFeatures features)
+        {
+            return $"Track ID: {features.Id}\n"
+                 + $"Acousticness: {features.Acousticness}\n"
+                 + $"Danceability: {features.Danceability}\n"
+                 + $"Energy: {features.Energy}\n"
+                 + $"Instrumentalness: {features.Instrumentalness}\n"
+                 + $"Liveness: {features.Liveness}\n"
+                 + $"Loudness: {features.Loudness}\n"
+                 + $"Speechiness: {features.Speechiness}\n"
+                 + $"Valence: {features.Valence}\n";
+        }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+        /// <summary>
+        /// Returns audio feature data for the track with the given ID.
+        /// </summary>
+        private static async Task<AudioFeatures> GetAudioFeatures(string trackId)
+        {
+            using var client = await GetClient();
+            return await client.GetAudioFeaturesAsync(trackId);
+        }
 
-            return new OkObjectResult(responseMessage);
+        /// <summary>
+        /// Returns an authenticated Spotify API client.
+        /// </summary>
+        private static async Task<SpotifyWebAPI> GetClient()
+        {
+            var auth = new CredentialsAuth(ClientId, ClientSecret);
+            var token = await auth.GetToken();
+
+            return new SpotifyWebAPI
+            {
+                AccessToken = token.AccessToken,
+                TokenType = token.TokenType
+            };
         }
     }
 }
