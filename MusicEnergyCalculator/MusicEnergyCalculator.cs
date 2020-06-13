99d0e85cf9a35bad83d33f0e81a7f94b9a002ec6
Julian Lawrance <julian.lawrance@gmail.com>
2020-06-13 16:10:33 +0100

    Return JSON response with a track's ID, audio features and per-day energy scores

--- MusicEnergyCalculator/MusicEnergyCalculator.cs
+++ MusicEnergyCalculator/MusicEnergyCalculator.cs
@@ -12,7 +12,14 @@ namespace MusicEnergyCalculator
 {
     public static class MusicEnergyCalculator
     {
+        /// <summary>
+        /// The app client ID.
+        /// </summary>
         private const string ClientId = "de8e2df8cbb84989bb42d13700e83168";
+
+        /// <summary>
+        /// The app client secret.
+        /// </summary>
         private const string ClientSecret = "b6ff30e7e1744da9ac435eda959fbbf4";
 
         [FunctionName("MusicEnergyCalculator")]
@@ -25,24 +32,96 @@ namespace MusicEnergyCalculator
             log.LogInformation($"Getting audio features for track: {trackId}");
 
             var features = await GetAudioFeatures(trackId);
-            var summary = GetSummary(features);
-            return new OkObjectResult(summary);
+            var response = GetTrackResponse(features);
+            return new OkObjectResult(response);
         }
 
         /// <summary>
-        /// Returns audio feature data for the track with the given ID.
+        /// Returns a track response for the given audio features.
         /// </summary>
-        private static string GetSummary(AudioFeatures features)
+        private static TrackResponse GetTrackResponse(AudioFeatures features)
         {
-            return $"Track ID: {features.Id}\n"
-                 + $"Acousticness: {features.Acousticness}\n"
-                 + $"Danceability: {features.Danceability}\n"
-                 + $"Energy: {features.Energy}\n"
-                 + $"Instrumentalness: {features.Instrumentalness}\n"
-                 + $"Liveness: {features.Liveness}\n"
-                 + $"Loudness: {features.Loudness}\n"
-                 + $"Speechiness: {features.Speechiness}\n"
-                 + $"Valence: {features.Valence}\n";
+            return new TrackResponse
+            {
+                Id = features.Id,
+
+                Acousticness = features.Acousticness,
+                Danceability = features.Danceability,
+                Energy = features.Energy,
+                Instrumentalness = features.Instrumentalness,
+                Liveness = features.Liveness,
+                NormalisedLoudness = features.NormalisedLoudness(),
+                Speechiness = features.Speechiness,
+                Valence = features.Valence,
+
+                MondayEnergy = GetMondayEnergyScore(features),
+                TuesdayEnergy = GetTuesdayEnergyScore(features),
+                WednesdayEnergy = GetWednesdayEnergyScore(features),
+                ThursdayEnergy = GetThursdayEnergyScore(features),
+                FridayEnergy = GetFridayEnergyScore(features),
+            };
+        }
+
+        /// <summary>
+        /// Returns a Monday energy score for the given audio features.
+        /// "experimental / noise / metal / grind / sixth world / misanthropic stuff"
+        /// </summary>
+        private static double GetMondayEnergyScore(AudioFeatures features)
+        {
+            return 0.3 * (1 - features.Valence) // misanthropic
+                 + 0.3 * (1 - features.Energy) // dispondent
+                 + 0.2 * features.NormalisedLoudness()
+                 + 0.1 * features.Instrumentalness
+                 + 0.1 * (1 - features.Danceability); // less danceable
+        }
+
+        /// <summary>
+        /// Returns a Tuesday energy score for the given audio features.
+        /// "techno / idm / glitch / illbient / deconstructed club / ambient / experimental"
+        /// </summary>
+        private static double GetTuesdayEnergyScore(AudioFeatures features)
+        {
+            return 0.4 * features.Instrumentalness
+                 + 0.3 * features.NormalisedLoudness()
+                 + 0.2 * features.Danceability
+                 + 0.1 * (1 - features.Energy);
+        }
+
+        /// <summary>
+        /// Returns a Wednesday energy score for the given audio features.
+        /// "ethereal / confident / uplifting / new age / majestic / orchestral / psychedelic"
+        /// </summary>
+        private static double GetWednesdayEnergyScore(AudioFeatures features)
+        {
+            return 0.3 * features.NormalisedLoudness()
+                 + 0.3 * features.Instrumentalness
+                 + 0.2 * features.Acousticness
+                 + 0.1 * features.Speechiness;
+        }
+
+        /// <summary>
+        /// Returns a Thursday energy score for the given audio features.
+        /// "joyous / confident / enigmatic / arrogant / charismatic"
+        /// </summary>
+        private static double GetThursdayEnergyScore(AudioFeatures features)
+        {
+            return 0.4 * features.Instrumentalness
+                 + 0.3 * features.NormalisedLoudness()
+                 + 0.2 * features.Danceability
+                 + 0.1 * features.Energy;
+        }
+
+        /// <summary>
+        /// Returns a Friday energy score for the given audio features.
+        /// "rave / hedonistic / party / unhinged / unstoppable"
+        /// </summary>
+        private static double GetFridayEnergyScore(AudioFeatures features)
+        {
+            return 0.4 * features.Energy
+                 + 0.25 * features.Danceability
+                 + 0.25 * features.Valence
+                 + 0.05 * features.NormalisedLoudness()
+                 + 0.05 * features.Liveness;
         }
 
         /// <summary>
@@ -67,6 +146,20 @@ namespace MusicEnergyCalculator
                 AccessToken = token.AccessToken,
                 TokenType = token.TokenType
             };
+        }
+    }
+
+    /// <summary>
+    /// Extension methods.
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Returns the loudness from the given features normalised to a value between 0 and 1.
+        /// </summary>
+        public static float NormalisedLoudness(this AudioFeatures features)
+        {
+            return (features.Loudness / 60) + 1;
         }
     }
 }
---
+++ MusicEnergyCalculator/TrackResponse.cs
@@ -0,0 +1,78 @@
+﻿namespace MusicEnergyCalculator
+{
+    /// <summary>
+    /// Model for responses.
+    /// </summary>
+    public class TrackResponse
+    {
+        /// <summary>
+        /// Gets or sets the ID of the track.
+        /// </summary>
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the acousticness of the track.
+        /// </summary>
+        public float Acousticness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the danceability of the track.
+        /// </summary>
+        public float Danceability { get; set; }
+
+        /// <summary>
+        /// Gets or sets the energy of the track.
+        /// </summary>
+        public float Energy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the instrumentalness of the track.
+        /// </summary>
+        public float Instrumentalness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the liveness of the track.
+        /// </summary>
+        public float Liveness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the normalised loudness of the track.
+        /// </summary>
+        public float NormalisedLoudness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the speechiness of the track.
+        /// </summary>
+        public float Speechiness { get; set; }
+
+        /// <summary>
+        /// Gets or sets the valence of the track.
+        /// </summary>
+        public float Valence { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Monday energy score of the track.
+        /// </summary>
+        public double MondayEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Tuesday energy score of the track.
+        /// </summary>
+        public double TuesdayEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Wednesday energy score of the track.
+        /// </summary>
+        public double WednesdayEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Thursday energy score of the track.
+        /// </summary>
+        public double ThursdayEnergy { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Friday energy score of the track.
+        /// </summary>
+        public double FridayEnergy { get; set; }
+    }
+}
